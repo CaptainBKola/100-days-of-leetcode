93a7d1c27fd2e4cc5b8e69a356228a584f5eaf67
captainbkola <badmuskola1989@gmail.com>
2022-11-04 10:41:24 +0100

    removed jargons

--- two_sum.py
+++ two_sum.py
@@ -1,26 +1,14 @@
-# def two_sum(arr, target):
-#     for first_element in range(len(arr)):
-#         for second_element in range( len(arr) + 1):
-    
-    
-#             return first_element, second_element
-#             # if first_element + second_element == target:
-#     #             first_index = arr[first_element]
-#     #         else:
-#     #             second_index = arr[second_element]
-    
-#     # return first_index, second_index
+"""
+Solution Tips
+We can actually complete it
+in a single pass. When iterating 
+and adding elements to the hash table 
+we additionally take a step back to see if
+the complement of the already added element 
+already exists there. If it does, 
+we have a solution and may instantly return the indices.
+"""
 
-# print(two_sum([1,2,4,3,3], 6))
-
-# arr = [23,2,3,3,4,3,2], 4
-# dict = {}
-# for i in range(len(arr)):
-#     if arr[i] in dict:
-#         dict[arr[i]] += 1
-#     else:
-#         dict[arr[i]] = 1
-# print(dict)
 def twoSum(nums,target):
     hashmap = {}
     for i in range(len(nums)):
@@ -29,3 +17,4 @@ def twoSum(nums,target):
             return [i, hashmap[complement]]
         hashmap[nums[i]] = i 
 print(twoSum([2,3,5,5,1],3))
+
